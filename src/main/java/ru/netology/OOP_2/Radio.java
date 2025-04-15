ab907b611383dc8345f108fa323755e5975f1f32
Naramakil <naramakil@gmail.com>
2025-04-15 22:31:41 +0300

    Initial commit

---
+++ src/main/java/ru/netology/OOP_2/Radio.java
@@ -0,0 +1,71 @@
+package ru.netology.OOP_2;
+
+public class Radio {
+    public int currentStation;
+    public int currentVolume;
+
+    public Radio() {
+        currentStation = 0; // Начальная станция
+        currentVolume = 50; // Начальная громкость
+    }
+
+    // Метод для переключения на следующую станцию
+    public void next() {
+        if (currentStation == 9) {
+            currentStation = 0;
+        } else {
+            currentStation++;
+        }
+    }
+
+    // Метод для переключения на предыдущую станцию
+    public void prev() {
+        if (currentStation == 0) {
+            currentStation = 9;
+        } else {
+            currentStation--;
+        }
+    }
+
+    // Метод для установки конкретной станции
+    public void setCurrentStation(int station) {
+        if (station < 0 || station > 9) {
+            return; // Не изменяем станцию, если номер недопустим
+        }
+        currentStation = station;
+    }
+
+    // Методы для управления громкостью
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume++;
+        } else currentVolume = 100;
+    }
+
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        } else currentVolume = 0;
+    }
+
+    public void setCurrentVolume(int volume) {
+        if (volume >= 0 && volume <= 100) {
+            currentVolume = volume;
+        } else if (volume < 0) {
+            currentVolume = 0; // Принудительно сбросить на минимум
+        } else {
+            currentVolume = 100; // Принудительно установить максимум
+        }
+    }
+
+    // Геттеры для получения текущих значений
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+}
---
+++ src/test/java/ru/netology/OOP_2/RadioTest.java
@@ -0,0 +1,123 @@
+package ru.netology.OOP_2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+
+    // Тесты для радиостанций
+    @Test
+    public void shouldSwitchToNextStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(5);
+        radio.next();
+        Assertions.assertEquals(6, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldSwitchToZeroIfMaxStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+        radio.next();
+        Assertions.assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldSwitchToPrevStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(5);
+        radio.prev();
+        Assertions.assertEquals(4, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldSwitchToMaxIfZeroStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+        radio.prev();
+        Assertions.assertEquals(9, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldSetStationWithinRange() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(5);
+        Assertions.assertEquals(5, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldNotSetStationBelowZero() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(-1);
+        Assertions.assertEquals(0, radio.getCurrentStation()); // или остаётся предыдущее значение, зависит от реализации
+    }
+
+    @Test
+    public void shouldNotSetStationAboveMax() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(10);
+        Assertions.assertEquals(0, radio.getCurrentStation()); // или остаётся предыдущее значение
+    }
+
+    // Тесты для громкости
+    @Test
+    public void shouldIncreaseVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(50);
+        radio.increaseVolume();
+        Assertions.assertEquals(51, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldNotIncreaseVolumeAboveMax() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(100);
+        radio.increaseVolume();
+        Assertions.assertEquals(100, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldDecreaseVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(50);
+        radio.decreaseVolume();
+        Assertions.assertEquals(49, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldSetMaxVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(100);
+        Assertions.assertEquals(100, radio.getCurrentVolume());
+
+    }
+
+    @Test
+    public void shouldSetVolumeAboveMax() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(101);
+        Assertions.assertEquals(100, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldSetMinVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+        Assertions.assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldSetVolumeBelowMin() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(-1);
+        Assertions.assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldNotDecreaseVolumeBelowMin() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+        radio.decreaseVolume();
+        Assertions.assertEquals(0, radio.getCurrentVolume());
+    }
+}
